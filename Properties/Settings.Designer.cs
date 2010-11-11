a671cf41ffe8914f53ba51b6576d4e268691b246
yuvalnv@gmail.com <yuvalnv@gmail.com@ad1bf302-fdf2-25e8-dedc-877b28622ad8>
2010-11-11 00:42:06 +0000

    Added ApplicationVersion setting - used for Automatic upgrade of previous user settings

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace BigMansStuff.PracticeSharp.UI.Properties {
                 this["LastFilename"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string ApplicationVersion {
+            get {
+                return ((string)(this["ApplicationVersion"]));
+            }
+            set {
+                this["ApplicationVersion"] = value;
+            }
+        }
     }
 }
