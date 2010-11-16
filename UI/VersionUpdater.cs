137745ffbc23778a58517bbfb27d6e1bd8dc3e79
yuvalnv@gmail.com <yuvalnv@gmail.com@ad1bf302-fdf2-25e8-dedc-877b28622ad8>
2010-11-16 02:49:14 +0000

    Improved version update - now asynchronous

--- UI/VersionUpdater.cs
+++ UI/VersionUpdater.cs
@@ -5,61 +5,128 @@ using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace BigMansStuff.PracticeSharp.UI
 {
     /// <summary>
     /// Utility class for updating a new version of Practice#
+    /// Performs a WebScrape of the Latest Version Wiki Page on Practice# Google Code site,
+    ///   and compares to the currently installed version
     /// </summary>
-    static class VersionUpdater
+    internal class VersionUpdater
     {
+        #region Construction
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mainForm"></param>
+        /// <param name="installedVersion"></param>
+        public VersionUpdater(Form mainForm, Version installedVersion)
+        {
+            m_mainForm = mainForm;
+            m_installedVersion = installedVersion;
+        }
+
+        #endregion
+
+        #region Public API
+
         /// <summary>
         /// Checks if a new Version of Practice# has been released
         /// </summary>
-        public static void CheckNewVersion( Version installedVersion )
+        public void CheckNewVersion()
         {
-            Version newVersion;
-            DateTime lastVersionCheckDateTime = Properties.Settings.Default.LastVersionCheckDateTime;
+            m_worker = new BackgroundWorker();
+            m_worker.DoWork += new DoWorkEventHandler(CheckNewVersionAsync_DoWork);
+            m_worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_RunWorkerCompleted);
+            m_worker.RunWorkerAsync();
+        }
 
-            DateTime nextCheckDateTime;
-            if (lastVersionCheckDateTime == DateTime.MinValue)
+        #endregion
+
+        #region Event Handlers
+
+
+        /// <summary>
+        /// Do the actual work of checking for a new version
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckNewVersionAsync_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
             {
-                lastVersionCheckDateTime = DateTime.Now;
-                nextCheckDateTime = DateTime.MinValue;
-            }
-            else
-            {
-                if (Properties.Settings.Default.SupressVersionCheck)
+                Version newVersion;
+                DateTime lastVersionCheckDateTime = Properties.Settings.Default.LastVersionCheckDateTime;
+
+                DateTime nextCheckDateTime;
+                if (lastVersionCheckDateTime == DateTime.MinValue)
                 {
-                    // Supressed Mode - Checks once a month
-                    nextCheckDateTime = lastVersionCheckDateTime.AddMonths(1);
+                    lastVersionCheckDateTime = DateTime.Now;
+                    nextCheckDateTime = DateTime.MinValue;
                 }
                 else
                 {
-                    // Regular mode - Checks once a week
-                    nextCheckDateTime = lastVersionCheckDateTime.AddDays(7);
-                }
-            }
-
-            if (
-                (lastVersionCheckDateTime != DateTime.MinValue && DateTime.Now >= nextCheckDateTime))
-            {
-                Properties.Settings.Default.LastVersionCheckDateTime = DateTime.Now;
-
-                if (VersionUpdater.IsNewVersionReleased(installedVersion, out newVersion))
-                {
-                    if (DialogResult.Yes == MessageBox.Show(string.Format(Resources.NewVersionFound, newVersion.ToString()), "Practice#", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    if (Properties.Settings.Default.SupressVersionCheck)
                     {
-                        System.Diagnostics.Process.Start("http://code.google.com/p/practicesharp/downloads/list");
+                        // Supressed Mode - Checks once a month
+                        nextCheckDateTime = lastVersionCheckDateTime.AddMonths(1);
                     }
                     else
                     {
-                        Properties.Settings.Default.SupressVersionCheck = true;
+                        // Regular mode - Checks once a week
+                        nextCheckDateTime = lastVersionCheckDateTime.AddDays(7);
+                    }
+                }
+
+                if ( lastVersionCheckDateTime != DateTime.MinValue && DateTime.Now >= nextCheckDateTime)
+                {
+                    Properties.Settings.Default.SupressVersionCheck = false;
+                    Properties.Settings.Default.Save();
+                
+                    // Check if the new version is actually newer than the installed version
+                    if (IsNewVersionReleased(out newVersion))
+                    {
+                        DialogResult dialogResult = DialogResult.None;
+                        if (m_mainForm.Disposing || m_mainForm.IsDisposed)
+                            return;
+                        m_mainForm.Invoke( new MethodInvoker( delegate() 
+                        {
+                            dialogResult = MessageBox.Show(m_mainForm, string.Format(Resources.NewVersionFound, newVersion.ToString()), "Practice#", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        } ) );
+
+                        if ( dialogResult == DialogResult.Yes )
+                        {
+                            // Launch the Practice# GoogleCode Downloads web page
+                            System.Diagnostics.Process.Start( DownloadsWebPageURL );
+                        }
+                        else
+                        {
+                            Properties.Settings.Default.SupressVersionCheck = true;
+                        }
+                        Properties.Settings.Default.LastVersionCheckDateTime = DateTime.Now;
+                        Properties.Settings.Default.Save();
                     }
                 }
             }
-
+            catch (Exception)
+            {
+                // Ignore errors, we're just checking for a new version - the internet connection might be off
+            }
         }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Worker has completed - clean up members
+            m_mainForm = null;
+            m_worker = null;
+            m_installedVersion = null;
+        }
+
+        #endregion
+
+        #region Private Methods
 
         /// <summary>
         /// Utility method that checks if a new version of Practice# has been released
@@ -67,43 +134,52 @@ namespace BigMansStuff.PracticeSharp.UI
         /// <param name="installedVersion"></param>
         /// <param name="newVersion"></param>
         /// <returns></returns>
-        public static bool IsNewVersionReleased( Version installedVersion, out Version newVersion )
+        private bool IsNewVersionReleased( out Version newVersion )
         {
             newVersion = null;
 
-            try
-            {
-                // Web Scrape the Google Code LatestVersion wiki page
-                string target = @"http://code.google.com/p/practicesharp/wiki/LatestVersion";
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(target);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Web Scrape the Google Code LatestVersion wiki page
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(LatestVersionWebPageURL);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-                using (Stream responseStream = response.GetResponseStream())
-                using (StreamReader htmlStream = new StreamReader(responseStream, Encoding.UTF8))
+            using (Stream responseStream = response.GetResponseStream())
+            using (StreamReader htmlStream = new StreamReader(responseStream, Encoding.UTF8))
+            {
+                string pageHtml = htmlStream.ReadToEnd();
+
+                // Match the latest version value by using a regular expression
+                Regex regEx = new Regex(@"Practice# latest Version is: (\d+.\d+.\d+.\d+)");
+                Match match = regEx.Match(pageHtml);
+
+                if (match != null)
                 {
-                    string pageHtml = htmlStream.ReadToEnd();
-
-                    // Match the latest version value by using a regular expression
-                    Regex regEx = new Regex(@"Practice# latest Version is: (\d+.\d+.\d+.\d+)");
-                    Match match = regEx.Match(pageHtml);
-
-                    if (match != null)
+                    newVersion = new Version(match.Groups[1].Value);
+                    if (newVersion > m_installedVersion)
                     {
-                        newVersion = new Version(match.Groups[1].Value);
-                        if (newVersion > installedVersion)
-                        {
-                            return true;
-                        }
+                        return true;
                     }
-
                 }
-            }
-            catch (Exception)
-            {
-                // Ignore errors, we're just checking for a new version - the internet connection might be off
-            }
 
+            }
+                
             return false; 
         }
+
+        #endregion
+
+        #region Private Members
+
+        private Form m_mainForm;
+        private Version m_installedVersion;
+        private BackgroundWorker m_worker;
+    
+        #endregion
+
+        #region Constants
+
+        private const string DownloadsWebPageURL = "http://code.google.com/p/practicesharp/downloads/list";
+        private const string LatestVersionWebPageURL = "http://code.google.com/p/practicesharp/wiki/LatestVersion";
+
+        #endregion
     }
 }
