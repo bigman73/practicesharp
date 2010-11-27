9c8e077baef29407144f2125af810ec4dea36f76
yuvalnv@gmail.com <yuvalnv@gmail.com@ad1bf302-fdf2-25e8-dedc-877b28622ad8>
2010-11-27 17:47:09 +0000

    Default namespace changed

--- PracticeSharpApp/Resources.Designer.cs
+++ PracticeSharpApp/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BigMansStuff.PracticeSharp.UI {
+namespace BigMansStuff.PracticeSharp {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BigMansStuff.PracticeSharp.UI {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BigMansStuff.PracticeSharp.UI.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BigMansStuff.PracticeSharp.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
